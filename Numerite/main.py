b55e8b399be5d3e918f677a8a041231f5a43d23c
Aditi Killedar <aditi.s.killedar@gmail.com>
2022-11-03 15:42:30 +0530

    Numerite - operation prediction integrated

--- Numerite/main.py
+++ Numerite/main.py
@@ -1,9 +1,18 @@
 '''main.py ensures calling of all modules in the order designed by our pipeline'''
+import OperatorPrediction as opred
 
 
 class AMWPS:
-    def __init__(self, mwp):
-        self.mwp = mwp
+        def __init__(self, mwp):
+                self.mwp = mwp
+                self.operator = None;
 
-    def identify_operation(self):
-        op =
+        def identify_operation(self):
+                self.operator = opred.predict_operation(self.mwp)
+
+if __name__ == '__main__':
+        inputmwp = 'In the fridge, there are 4 stacks of chocolate puddings and 5 stacks of pasta salad. How many stacks of dessert are there?'
+        test_mwp = AMWPS(inputmwp)
+
+        test_mwp.identify_operation()
+        print(test_mwp.operator)
