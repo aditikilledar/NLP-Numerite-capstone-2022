ea21ea5cd0a027eb3c39291cfb0af24b78fed8c9
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2022-11-04 17:56:29 +0530

    knowledge base

--- Numerite/main.py
+++ Numerite/main.py
@@ -1,5 +1,7 @@
 '''main.py ensures calling of all modules in the order designed by our pipeline'''
 import OperatorPrediction as opred
+import nltk
+import spacy
 from Microstatements import MicroStatments as microstats
 
 class AMWPS:
@@ -10,20 +12,47 @@ class AMWPS:
                 self.solution = None
                 self.kb = []
                 self.equation = ''
+        
         def identify_operation(self):
                 self.operator = opred.predict_operation(self.mwp)
+        
         def get_microstatements(self):
             ms = microstats()
             self.microstatements = ms.mwp_split(self.mwp)
+        
         def KB(self):
-            # TODO
-            pass
+            ner = spacy.load("en_core_web_sm")
+            res = []
+            for statement in self.microstatements: # ignore question microstatement
+                parsed = ner(statement)
+                owner = ''
+                quantity = ''
+                obj = ''
+                
+                for word in parsed.ents:
+                #print(word.label_, word.text, i)
+                    if word.label_ == "CARDINAL":
+                        quantity = word.text
+                    elif word.label_ == "PERSON" or word.label == "ORG":
+                        owner = word.text
+                    
+                    words = nltk.word_tokenize(statement)
+                    for i, w in enumerate(words):
+                        if w.isnumeric():
+                            obj = words[i + 1]
+
+                    res.append((obj, owner, quantity))
+            
+            self.kb = res
+
         def get_equation(self):
             # TODO
             pass
+        
         def solve_equation(self):
             # TODO
             pass
+        
         def solve(self):
             self.get_microstatements()
             self.identify_operation()
