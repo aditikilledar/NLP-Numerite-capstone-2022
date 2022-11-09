15b5b1d431465dc9367af4e104fa13296ca74dc0
Gautham YS <66833388+gauthamys@users.noreply.github.com>
2022-11-09 10:43:52 +0530

    extract quants

--- Numerite/util.py
+++ Numerite/util.py
@@ -1,29 +1,17 @@
 import nltk
 def extractAll(txt):
     wordsList = nltk.word_tokenize(txt)
-
-
     #wordsList = [w for w in wordsList if not w in stop_words]
-
-
+    
     tagged = nltk.pos_tag(wordsList)
     # determining the units in the word problem 
+    
     unit = [""]
     for i in range(len(tagged)-2):
-
         if tagged[i][1] == "CD":
-
-
-
-
             if tagged[i+1][1] == "NN" or tagged[i+1][1]=='NNP'  or  tagged[i+1][1]=='NNS' or tagged[i+1][1]=='NNPS' :
-
-            
-            
                 unit.append(tagged[i+1])
-            
             elif tagged[i+2][1]=='NN' or tagged[i+2][1]=='NNP' or tagged[i+2][1]=='NNS' or tagged[i+2=='NNPS']:
-            
                 unit.append(tagged[i+2])
 
     #appending units 
@@ -37,9 +25,8 @@ def extractAll(txt):
     #extracting quantities
     for i in range(len(tagged)-2):
         if tagged[i][1] == "CD":
-
             if tagged[i+1][1] == "NN" or tagged[i+1][1]=='NNP'or tagged[i+1][1]=='NNPS' or tagged[i+2][1]=='NN' or tagged[i+2][1]=='NNP'or tagged[i+2][1]=='NNPS' or  tagged[i+1][1]=='NNS' or tagged[i+2][1]=='NNS' :
-            
                 s = tagged[i][0] + " "+ tagged[i+1][0]
                 entities.append(s)
+    
     return(entities)
